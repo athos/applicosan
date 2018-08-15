74ccb183c74ebf40c6056d88612ca33032a34261
Shogo Ohta <athos0220@gmail.com>
2018-08-15 12:51:19 +0900

    Implement Slack URL verification

--- src/apprentice/controllers.clj
+++ src/apprentice/controllers.clj
@@ -8,10 +8,16 @@
       (res/response (str challenge "." response))
       (res/not-found "Not Found"))))
 
+(defn slack [{{:keys [type] :as params} :params}]
+  (case type
+    "url_verification" (res/response {:challenge (:challenge params)})
+    nil))
+
 (defn hello [{:keys [headers body params] :as req}]
   (res/response {:resp (str "Hello, " (or (:name params) "World") "!")}))
 
 (defmethod ig/init-key :app/controllers [_ {:keys [env]}]
   (let [acme-challenge (get env :acme-challenge)]
     {:acme (acme acme-challenge)
+     :slack slack
      :hello hello}))
--- src/apprentice/routes.clj
+++ src/apprentice/routes.clj
@@ -3,4 +3,5 @@
 
 (defmethod ig/init-key :app/routes [_ _]
   '{[:get "/.well-known/acme-challenge/" challenge] [:acme challenge]
+    [:post "/slack"] ^:api [:slack]
     "/hello" ^:api [:hello]})
