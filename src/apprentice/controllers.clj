e3403baca10d0b0b2196f213a1c286dae6d59a0c
Shogo Ohta <athos0220@gmail.com>
2018-08-17 00:25:52 +0900

    Integrate controller with rule system

--- src/apprentice/controllers.clj
+++ src/apprentice/controllers.clj
@@ -1,5 +1,7 @@
 (ns apprentice.controllers
-  (:require [apprentice.slack :as slack]
+  (:require [apprentice.rules.core :as rules]
+            [apprentice.rules]
+            [clojure.string :as str]
             [integrant.core :as ig]
             [ring.util.response :as res]))
 
@@ -9,21 +11,24 @@
       (res/response (str challenge "." response))
       (res/not-found "Not Found"))))
 
-(defn slack-event-handler [client]
+(defn handle-mention [event {:keys [slack] :as opts}]
+  (when (and (not= (:user event) (:id slack))
+             (not= (:username event) (:name slack)))
+    (let [message (str/replace (:text event) (str "<@" (:id slack) "> ") "")]
+      (rules/apply-rule message event opts))))
+
+(defn slack-event-handler [opts]
   (fn [{{:keys [type] :as params} :params}]
     (case type
       "url_verification" (res/response {:challenge (:challenge params)})
       "event_callback" (let [{:keys [type] :as event} (:event params)]
                          (case type
-                           "app_mention" (do
-                                           (when (and (not= (:user event) (:id client))
-                                                      (not= (:username event) (:name client)))
-                                             (slack/post-message client (:channel event) (:text event)))
-                                           (res/response "ok"))
-                           (res/response "ok")))
+                           "app_mention" (handle-mention event opts)
+                           nil)
+                         (res/response "ok"))
       (res/response "ok"))))
 
-(defmethod ig/init-key :app/controllers [_ {:keys [slack env]}]
+(defmethod ig/init-key :app/controllers [_ {:keys [env] :as opts}]
   (let [acme-challenge (get env :acme-challenge)]
     {:acme (acme acme-challenge)
-     :slack (slack-event-handler slack)}))
+     :slack (slack-event-handler opts)}))
