7d985544002d52eb6ac01c8dc4c3ad6d335b69c3
Shogo Ohta <athos0220@gmail.com>
2018-08-17 08:53:08 +0900

    Update conversation rules

--- src/apprentice/rules.clj
+++ src/apprentice/rules.clj
@@ -5,5 +5,8 @@
 (defn reply [{:keys [channel]} {:keys [slack]} message]
   (slack/post-message slack channel message))
 
-(defrule hello #"^(?:hi)|(?:hello)" [event opts]
-  (reply event opts "Hi"))
+(defrule hello #"^hi|hello|おはよう|こんにちは" [event opts]
+  (reply event opts "おはよー☀️"))
+
+(defrule bye #"^bye|goodbye|さようなら|ばいばい" [event opts]
+  (reply event opts "ばいばいー👋"))
