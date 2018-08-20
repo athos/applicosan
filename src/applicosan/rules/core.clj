b4b7bfd8c84c20aaf12e788bdf162ef0fae5ead7
Shogo Ohta <athos0220@gmail.com>
2018-08-20 14:19:57 +0900

    Refine defrule macro to improve testability for each rule

--- src/applicosan/rules/core.clj
+++ src/applicosan/rules/core.clj
@@ -12,14 +12,16 @@
     name))
 
 (defmacro defrule [rule-name pattern [event & args] & body]
-  `(add-rule! defined-rules
-              (symbol (name (ns-name *ns*)) (name '~rule-name))
-              ~pattern
-              (fn [~event ~'&match ~@args] ~@body)))
+  `(do
+     (defn ~rule-name [~'&match ~event ~@args] ~@body)
+     (add-rule! defined-rules
+                (symbol (name (ns-name *ns*)) (name '~rule-name))
+                ~pattern
+                #'~rule-name)))
 
 (defn apply-rule [message event & args]
   (reduce (fn [_ {:keys [pattern action] :as rule}]
             (when-let [match (re-find pattern message)]
-              (reduced (apply action event match args))))
+              (reduced (apply action match event args))))
           nil
           @defined-rules))
