bcbfa1ea2fba2266c22e1393d8f882786c721622
Shogo Ohta <athos0220@gmail.com>
2018-08-20 23:19:59 +0900

    Add another command to check overtime

--- src/applicosan/rules.clj
+++ src/applicosan/rules.clj
@@ -12,7 +12,14 @@
     (format "%d時間%d分" (long (quot t 60)) (long (mod t 60)))
     (format "%d分" (long t))))
 
-(defrule hello #"^hi|hello|おはよう|こんにちは" [event {:keys [db] :as opts}]
+(defn notify-overtime [event {:keys [db] :as opts} & {:keys [excludes-today?]}]
+  (let [{:keys [last total]} (worktime/aggregate-overtime db)]
+    (reply event opts
+           (cond->> (str "今月の残業時間は" (stringify-time total) "だよ")
+             (not excludes-today?)
+             (str "今日の残業時間は" (stringify-time last) "、")))))
+
+(defrule hello #"^hi|hello|おは|こんにちは" [event {:keys [db] :as opts}]
   (worktime/clock-in! db)
   (let [{:keys [total]} (worktime/aggregate-overtime db)]
     (reply event opts "おはよー☀️")
@@ -24,15 +31,17 @@
     (worktime/clock-in! db clockin-time)
     (reply event opts (str "出社時間を" hours ":" minutes "で記録したよ！"))))
 
-(defrule bye #"^bye|goodbye|さようなら|ばいばい" [event {:keys [db] :as opts}]
+(defrule bye #"^bye|goodbye|さようなら|ばいばい|おつかれ|お疲れ" [event {:keys [db] :as opts}]
   (worktime/clock-out! db)
-  (let [{:keys [last total]} (worktime/aggregate-overtime db)]
-    (reply event opts "おつかれさまー👋")
-    (reply event opts (str "今日の残業時間は" (stringify-time last) "、"
-                           "今月の残業時間は" (stringify-time total) "だよ"))))
+  (reply event opts "おつかれさまー👋")
+  (notify-overtime event opts))
 
 (defrule clock-out #"(\d{1,2}):(\d{1,2})退社" [event {:keys [db] :as opts}]
   (let [[_ hours minutes] &match
         clockout-time (time/today (Long/parseLong hours) (Long/parseLong minutes))]
     (worktime/clock-out! db clockout-time)
-    (reply event opts (str "退社時間を" hours ":" minutes "で記録したよ！"))))
+    (reply event opts (str "退社時間を" hours ":" minutes "で記録したよ！"))
+    (notify-overtime event opts)))
+
+(defrule check-overtime #"残業時間を?(?:確認|教えて)" [event opts]
+  (notify-overtime event opts :excludes-today? true))
