a5bcb10b758eed06a3c5a57ec83503aa9828a40e
Shogo Ohta <athos0220@gmail.com>
2018-08-31 22:27:23 +0900

    Add API for generating BufferedImage

--- src/applicosan/image.clj
+++ src/applicosan/image.clj
@@ -1,9 +1,25 @@
 (ns applicosan.image
-  (:import [java.awt.image BufferedImage]
+  (:import [java.awt Graphics2D RenderingHints]
+           [java.awt.image BufferedImage]
            [java.io ByteArrayOutputStream]
            [javax.imageio ImageIO]))
 
 (set! *warn-on-reflection* true)
+
+(defn- enable-antialiasing [^Graphics2D g]
+  (doto g
+    (.setRenderingHint RenderingHints/KEY_ANTIALIASING
+                       RenderingHints/VALUE_ANTIALIAS_ON)
+    (.setRenderingHint RenderingHints/KEY_TEXT_ANTIALIASING
+                       RenderingHints/VALUE_TEXT_ANTIALIAS_ON)))
+
+(defn ^BufferedImage generate-image [width height render-fn]
+  (let [image (BufferedImage. width height BufferedImage/TYPE_INT_ARGB)
+        g (.getGraphics image)]
+    (enable-antialiasing g)
+    (render-fn g)
+    (.dispose g)
+    image))
 
 (defn ->bytes [^BufferedImage image]
   (let [baos (ByteArrayOutputStream.)]
