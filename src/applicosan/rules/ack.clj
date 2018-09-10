532649e1ea0e26a4e916bb3935d8d33b869270c4
Shogo Ohta <athos0220@gmail.com>
2018-09-10 09:40:56 +0900

    Add new rule for acknowledgement

---
+++ src/applicosan/rules/ack.clj
@@ -0,0 +1,13 @@
+(ns applicosan.rules.ack
+  (:require [applicosan.rules.core :as rules :refer [defrule]]
+            [applicosan.rules.utils :as utils]
+            [applicosan.slack :as slack]
+            [duct.logger :as logger]
+            [integrant.core :as ig]))
+
+(defrule ping #"^ping" [event {:keys [logger] :as opts}]
+  (logger/log logger :info ::ping)
+  (utils/reply event opts "pong"))
+
+(defmethod ig/init-key :applicosan.rules/ack [_ opts]
+  (rules/->rule-set opts [ping]))
