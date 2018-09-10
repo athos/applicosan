cdf39dc78403c029ac4e18331e908447a3928308
Shogo Ohta <athos0220@gmail.com>
2018-09-10 22:41:29 +0900

    Remove unnecessary routes ns

--- src/applicosan/routes.clj
+++
@@ -1,6 +0,0 @@
-(ns applicosan.routes
-  (:require [integrant.core :as ig]))
-
-(defmethod ig/init-key :applicosan/routes [_ _]
-  '{[:get "/.well-known/acme-challenge/" challenge] [:acme challenge]
-    [:post "/slack"] ^:api [:slack]})
